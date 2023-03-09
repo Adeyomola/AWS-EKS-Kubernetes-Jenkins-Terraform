f914f0be8a6ab3aa5130ffdec840bf85250e314b
Adeyomola <kayzeel@yahoo.com>
2023-03-09 19:03:51 +0000

    First Commit

---
+++ deployment/kubernetes.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.48.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.1"
+    }
+  }
+}
+
+data "terraform_remote_state" "eks" {
+  backend = "local"
+
+  config = {
+    path = "../learn-terraform-provision-eks-cluster/terraform.tfstate"
+  }
+}
+
+# Retrieve EKS cluster information
+provider "aws" {
+  region = data.terraform_remote_state.eks.outputs.region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.aws_eks_cluster.cluster.name
+    ]
+  }
+}
+
+resource "kubernetes_manifest" "app" {  
+manifest = yamldecode(file(app.yml))
+}
+
+resource "kubernetes_manifest" "db" {
+manifest = yamldecode(file(mongo.yml))
+}
+
+resource "kubernetes_manifest" "db-config-map" {
+manifest = yamldecode(file(mongo-config.yml))
+}
+
+resource "kubernetes_manifest" "secrets" {
+manifest = yamldecode(file(mongo-secret.yml))
+}
