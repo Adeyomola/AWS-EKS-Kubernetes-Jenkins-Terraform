f63e993655168c6bd240b546465112a760befdea
Adeyomola <kayzeel@yahoo.com>
2023-03-14 11:31:10 +0000

    Update jenkins user_data

--- jenkins/jenkins.tf
+++ jenkins/jenkins.tf
@@ -1,18 +1,21 @@
-resource "aws_ec2_instance" "jenkins" {
+resource "aws_instance" "jenkins" {
   instance_type               = "t2.micro"
   ami                         = "ami-0574da719dca65348"
-  subnet_id                   = aws_subnet.triserver_subnet["1"].id
-  vpc_security_group_ids      = [aws_security_group.triserver_sg.id]
+  vpc_security_group_ids      = [aws_security_group.jenkins_sg.id]
   user_data                   = file("user_data.sh")
   associate_public_ip_address = true
   key_name                    = "windows11"
 
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_security_group" "jenkins_sg" {
-  name        = "triserver_sg"
+  name        = "jenkins_sg"
   description = "allow incoming traffic on SSH"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 22
--- jenkins/user_data.sh
+++ jenkins/user_data.sh
@@ -8,7 +8,7 @@ sudo apt install openjdk-11-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
 sudo echo "deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/" > /etc/apt/sources.list.d/jenkins.list
 sudo apt update
-sudo apt install jenkins
+sudo apt install jenkins -y
 sudo systemctl start jenkins.service
 
 #install Terraform
