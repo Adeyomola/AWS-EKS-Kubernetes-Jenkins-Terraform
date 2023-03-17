4f05b6d804e394c32a1de70f116ad165b60e4d75
Adeyomola <kayzeel@yahoo.com>
2023-03-17 18:03:08 +0000

    Uncommented AWS and Unzip

--- jenkins/user_data.sh
+++ jenkins/user_data.sh
@@ -37,12 +37,12 @@ sudo apt-get update
 sudo apt-get install -y kubectl
 
 ##install unzip
-#sudo apt install unzip -y
+sudo apt install unzip -y
 
 #install aws-cli
-#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-#unzip awscliv2.zip
-#sudo ./aws/install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 # install ansible
 sudo apt update
