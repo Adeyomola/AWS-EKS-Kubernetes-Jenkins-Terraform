0e18ccf4b692e794ebd42db1de2077f21ad7bde4
Adeyomola <kayzeel@yahoo.com>
2023-03-18 20:07:38 +0000

    Increased number of nodes

--- provision/modules.tf
+++ provision/modules.tf
@@ -49,8 +49,8 @@ module "eks" {
       name           = "node-group-1"
       instance_types = ["t3.medium"]
       min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+      max_size       = 4
+      desired_size   = 3
     }
 
     two = {
