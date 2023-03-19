fddfd234a456fe9979975ef8f6e8b20568cd4819
adeyomola <kayzeel@yahoo.com>
2023-03-19 13:07:22 +0000

    Corrected error

--- deploy/route53.tf
+++ deploy/route53.tf
@@ -2,26 +2,18 @@ data "aws_route53_zone" "selected" {
   name = var.domain_name
 }
 
-resource "aws_route53_record" "record" {
+resource "aws_route53_record" "profilr" {
   zone_id = data.aws_route53_zone.selected.zone_id
-  name    = var.domain_name
+  name    = "profilr.${var.domain_name}"
   type    = "CNAME"
   ttl     = "300"
-  records = [data.kubernetes_ingress_v1.ingress.status.0.load_balancer.0.ingress.0.hostname]
+  records = [data.kubernetes_service_v1.lb.status.0.load_balancer.0.ingress.0.hostname]
 }
 
-#resource "aws_route53_record" "profilr" {
-#  zone_id = data.aws_route53_zone.selected.zone_id
-#  name    = "profilr.${var.domain_name}"
-#  type    = "CNAME"
-#  ttl     = "300"
-#  records = [data.kubernetes_service_v1.lb.status.0.load_balancer.0.ingress.0.hostname]
-#}
-#
-#resource "aws_route53_record" "sock-shop" {
-#  zone_id = data.aws_route53_zone.selected.zone_id
-#  name    = "sock-shop.${var.domain_name}"
-#  type    = "CNAME"
-#  ttl     = "300"
-#  records = [data.kubernetes_service_v1.lb-sock-shop.status.0.load_balancer.0.ingress.0.hostname]
-#}
+resource "aws_route53_record" "sock-shop" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "sock-shop.${var.domain_name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [data.kubernetes_service_v1.lb-sock-shop.status.0.load_balancer.0.ingress.0.hostname]
+}
