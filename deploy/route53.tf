c94a86984d9033e563337a6998ae04ea615cd053
adeyomola <kayzeel@yahoo.com>
2023-03-19 13:05:56 +0000

    Reverted to domain only

--- deploy/route53.tf
+++ deploy/route53.tf
@@ -2,18 +2,26 @@ data "aws_route53_zone" "selected" {
   name = var.domain_name
 }
 
-resource "aws_route53_record" "profilr" {
+resource "aws_route53_record" "record" {
   zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "profilr.${var.domain_name}"
+  name    = var.domain_name
   type    = "CNAME"
   ttl     = "300"
-  records = [data.kubernetes_service_v1.lb.status.0.load_balancer.0.ingress.0.hostname]
+  records = [data.kubernetes_ingress_v1.ingress.status.0.load_balancer.0.ingress.0.hostname]
 }
 
-resource "aws_route53_record" "sock-shop" {
-  zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "sock-shop.${var.domain_name}"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [data.kubernetes_service_v1.lb-sock-shop.status.0.load_balancer.0.ingress.0.hostname]
-}
+#resource "aws_route53_record" "profilr" {
+#  zone_id = data.aws_route53_zone.selected.zone_id
+#  name    = "profilr.${var.domain_name}"
+#  type    = "CNAME"
+#  ttl     = "300"
+#  records = [data.kubernetes_service_v1.lb.status.0.load_balancer.0.ingress.0.hostname]
+#}
+#
+#resource "aws_route53_record" "sock-shop" {
+#  zone_id = data.aws_route53_zone.selected.zone_id
+#  name    = "sock-shop.${var.domain_name}"
+#  type    = "CNAME"
+#  ttl     = "300"
+#  records = [data.kubernetes_service_v1.lb-sock-shop.status.0.load_balancer.0.ingress.0.hostname]
+#}
--- ssl/resources.tf
+++ ssl/resources.tf
@@ -8,21 +8,9 @@ resource "acme_registration" "reg" {
 }
 
 resource "acme_certificate" "certificate" {
-account_key_pem = acme_registration.reg.account_key_pem
-  common_name     = "${var.namespaces[1]}.${var.domain_name}"
-  dns_challenge {
-    provider = "route53"
-  }
-}
-
-resource "acme_registration" "reg2" {
-  account_key_pem = tls_private_key.private_key.private_key_pem
-  email_address   = var.email
-}
-
-resource "acme_certificate" "certificate2" {
-  account_key_pem = acme_registration.reg2.account_key_pem
-  common_name     = "${var.namespaces[0]}.${var.domain_name}"
+  account_key_pem           = acme_registration.reg.account_key_pem
+  common_name               = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
   dns_challenge {
     provider = "route53"
   }
