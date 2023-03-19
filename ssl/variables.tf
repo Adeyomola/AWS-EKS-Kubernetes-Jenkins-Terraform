54a1eab9c9c641d520ec818e37f2ebe98b59a989
adeyomola <kayzeel@yahoo.com>
2023-03-19 01:25:32 +0000

    Corrected incorrect variable declaration

--- ssl/variables.tf
+++ ssl/variables.tf
@@ -1,9 +1,9 @@
 variable "email" {}
 variable "table_name" {
   description = "Dynamo DB Table Name"
-  value       = "adeyomola_dynamodb"
+  default       = "adeyomola_dynamodb"
 }
 variable "bucket_name" {
   description = "S3 Bucket Name"
-  value       = "adeyomola-tfstate-bucket"
+  default     = "adeyomola-tfstate-bucket"
 }
