f75202158645b8dba120c0bead280ed0dfc19778
adeyomola <kayzeel@yahoo.com>
2023-03-19 09:03:12 +0000

    Adjusted ssl module

--- ssl/variables.tf
+++ ssl/variables.tf
@@ -1,16 +1,11 @@
 variable "email" {}
 variable "table_name" {
   description = "Dynamo DB Table Name"
-  default       = "adeyomola_dynamodb"
+  default     = "adeyomola_dynamodb"
 }
 variable "bucket_name" {
   description = "S3 Bucket Name"
   default     = "adeyomola-tfstate-bucket"
-}
-
-variable "namespaces" {
-  description = "namespaces list"
-  default     = ["profilr", "sock-shop"]
 }
 
 variable "domain_name" {
