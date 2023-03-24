7331a86e209ec8b778782519a8b06329c243978e
adeyomola <kayzeel@yahoo.com>
2023-03-24 20:36:32 +0000

    .

--- deploy/helm.tf
+++ deploy/helm.tf
@@ -39,7 +39,6 @@ resource "helm_release" "kibana" {
   name       = "kibana"
   namespace  = "kube-system"
   repository = "https://helm.elastic.co"
-  version = "~> 8.4.0"
   chart      = "kibana"
   wait       = false
   values     = ["${file("./values/kibana.yml")}"]
